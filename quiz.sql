7e794b488140203e271e92306b780fdc4e6bf0b2
SuperB-oris <64464526+SuperB-oris@users.noreply.github.com>
2021-04-26 12:48:20 +0800

    v1.0 Schema

---
+++ quiz.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2021 at 04:47 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quiz`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `question_id` int(2) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `c1` varchar(200) NOT NULL,
+  `c2` varchar(200) NOT NULL,
+  `c3` varchar(200) NOT NULL,
+  `c4` varchar(200) NOT NULL,
+  `correct_q` varchar(2) NOT NULL,
+  `room_id` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `room_id` int(4) NOT NULL,
+  `inv_code` varchar(20) NOT NULL,
+  `max_player` int(2) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `current_q` int(2) NOT NULL,
+  `list_of_player` varchar(200) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(4) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `created_at`) VALUES
+(2529, 'abc', '2021-04-26'),
+(4841, 'abc', '2021-04-26'),
+(4222, 'abc', '2021-04-26');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
