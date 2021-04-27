974b01033bf0d7658b8443bba88a722cbe20e657
SuperB-oris <64464526+SuperB-oris@users.noreply.github.com>
2021-04-27 17:01:52 +0800

    Update user.php

--- php/function/user.php
+++ php/function/user.php
@@ -2,13 +2,85 @@
 
 function create_user($username)
 {
-  $user_id = rand(1000, 9999);
-  $created_at = date('Y-m-d H:i:s');
-  $sql = "INSERT INTO user(user_id, name, created_at) VALUES ($user_id,'$username','$created_at')";
+  try {
+    $user_id = rand(1000, 9999);
+    $created_at = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO user(user_id, name, created_at) VALUES ($user_id,'$username','$created_at')";
 
-  $db = new db();
-  $db = $db->connect();
-  $stmt = $db->prepare($sql);
-  $stmt->execute();
-  $db = null;
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $db = null;
+  } catch (PDOException $e) {
+    return msgPack("failed", $e);
+  }
+  return msgPack("success", $user_id);
 }
+
+function get_player($room_id)
+//{"room_id":"3395"}
+{
+
+  $sql = "SELECT * FROM user WHERE room_id = '$room_id'";
+  try {
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->query($sql);
+    $fetched = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    return msgPack("success", $fetched);
+  } catch (PDOException $e) {
+    return msgPack("failed", $e);
+  }
+}
+
+function assign_player($room_id, $user_id)
+//{"room_id":"3395", "user_id":"2529"}
+{
+  if (checkuser($user_id) != false) {
+    if (checkroom($room_id) != false) {
+      $sql = "UPDATE user SET room_id='$room_id' WHERE user_id = '$user_id'";
+      try {
+        $db = new db();
+        $db = $db->connect();
+        $stmt = $db->prepare($sql);
+        $fetched = $stmt->execute();
+        $db = null;
+        return msgPack("success", $fetched);
+      } catch (PDOException $e) {
+        return msgPack("failed", $e);
+      }
+    }
+  }
+}
+
+function checkuser($user_id)
+{
+  try {
+    $sql = "SELECT * FROM user WHERE user_id = $user_id";
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->query($sql);
+    $fetched = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    return msgPack("success", $fetched);
+  } catch (PDOException $e) {
+    return msgPack("failed", 'The user does not exist');
+  }
+};
+
+function checkroom($room_id)
+{
+  try {
+    $sql = "SELECT * FROM room WHERE room_id = $room_id";
+    $db = new db();
+    $db = $db->connect();
+    $stmt = $db->query($sql);
+    $fetched = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    return msgPack("success", $fetched);
+  } catch (PDOException $e) {
+    return msgPack("failed", 'The room does not exist');
+  }
+};
